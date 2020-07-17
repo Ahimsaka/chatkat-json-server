cf0d6b66df2c72f168a6995db4154c2bf6fca892
Ahimsaka <devinnarnold@gmail.com>
2020-07-16 17:09:45 -0700

    Added time-frame parameters.

--- src/main/java/com/chatkat/jsonserver/controller/ChatKatRestController.java
+++ src/main/java/com/chatkat/jsonserver/controller/ChatKatRestController.java
@@ -35,31 +35,34 @@ class ChatKatRestController {
     }
 
     @GetMapping("/guild/{id}")
-    public Guild guild(@PathVariable("id") final  long id, @RequestParam(value = "last", defaultValue="") final String last) {
-        return guildService.getById(id);
+    public Guild guild(@PathVariable("id") final  long id,
+                       @RequestParam(value = "unit", defaultValue="") final String unit,
+                       @RequestParam(value = "multiple", defaultValue="1") final int multiple) {
+        return guildService.getById(id, setInterval(unit, multiple));
     }
 
     @GetMapping("/channel/{id}")
-    public Channel channel(@PathVariable("id") final long id, @RequestParam(value = "last", defaultValue="") final String last) {
-        return channelService.getById(id);
+    public Channel channel(@PathVariable("id") final long id,
+                           @RequestParam(value = "unit", defaultValue="") final String unit,
+                           @RequestParam(value = "multiple", defaultValue="1") final int multiple) {
+        return channelService.getById(id, setInterval(unit, multiple));
     }
 
     // set long for time-frame based search
-    private String setInterval(String last) {
-        if (last.equals("")) return last;
+    private long setInterval(final String unit, final int multiple) {
+        if (unit.equals("")) return 0;
         /*  create HashMap for parsing time parameters. this is done inside the method to ensure that
          *  ZonedDateTime.now() equates to the moment of the request (or slightly after) */
         HashMap<String, Instant> setInterval = new HashMap<>(4) {{
-            put("year", ZonedDateTime.now().minusYears(1).toInstant());
-            put("month", ZonedDateTime.now().minusMonths(1).toInstant());
-            put("week", ZonedDateTime.now().minusWeeks(1).toInstant());
-            put("day", ZonedDateTime.now().minusDays(1).toInstant());
+            put("year", ZonedDateTime.now().minusYears(multiple).toInstant());
+            put("month", ZonedDateTime.now().minusMonths(multiple).toInstant());
+            put("week", ZonedDateTime.now().minusWeeks(multiple).toInstant());
+            put("day", ZonedDateTime.now().minusDays(multiple).toInstant());
         }};
         // check request message for time range parameters
         long interval = 0; // set default interval to Epoch to retrieve full history if no params found.
-        if (setInterval.containsKey(last.toLowerCase()))
-            interval = setInterval.get(last.toLowerCase()).toEpochMilli();
-        if (interval == 0) return "";
-        return String.format("WHERE time>=%d ", interval);
+        if (setInterval.containsKey(unit.toLowerCase()))
+            interval = setInterval.get(unit.toLowerCase()).toEpochMilli();
+        return interval;
     }
 }
